12dcaa8a18b8408c04f679ea0fed89f9e9ee485d
Matheus Vitorino Ribeiro <matheus.vitorino@saude.gov>
2025-11-10 15:06:43 -0300

    Fazendo os gráficos criados serem mantidos após atualizações

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -3,6 +3,4 @@ from .pca import PCA
 from .qualificacao import Qualificacao
 from .licitacao import Licitacao
 from .access_request import AccessRequest
-from .user_dashboard import UserDashboard
-
-__all__ = ["Usuario", "PCA", "Qualificacao", "Licitacao", "AccessRequest", "UserDashboard"]
+__all__ = ["Usuario", "PCA", "Qualificacao", "Licitacao", "AccessRequest"]
